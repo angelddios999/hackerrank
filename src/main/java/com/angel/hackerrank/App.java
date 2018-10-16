619bb1daa171ef908475bfed4a13fa508b11fb88
Angel De Dios <angel@MacBook-Pro-de-Angel.local>
2018-10-15 23:01:56 -0700

    New Files

---
+++ src/main/java/com/angel/hackerrank/App.java
@@ -0,0 +1,14 @@
+package com.angel.hackerrank;
+
+/**
+ * for every index compare with the ones in the right until a greater number is found
+ * if no greater number is found then swap current number to last position
+ * if greater is found swap position with the one previous to it
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/angel/hackerrank/SockMerchant.java
@@ -0,0 +1,17 @@
+package com.angel.hackerrank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class SockMerchant {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+        return 0;
+    }
+}
