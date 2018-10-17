a057ab3a524587f5e839bc1ceb4742a8c3e3e83a
Angel De Dios <angel@MacBook-Pro-de-Angel.local>
2018-10-16 22:03:40 -0700

    Completed SockMerchant

--- src/main/java/com/angel/hackerrank/SockMerchant.java
+++ src/main/java/com/angel/hackerrank/SockMerchant.java
@@ -12,6 +12,21 @@ public class SockMerchant {
 
     // Complete the sockMerchant function below.
     static int sockMerchant(int n, int[] ar) {
-        return 3;
+        int result = 0;
+        if (Objects.nonNull(ar)) {
+            Map<Integer, Integer> colorsMap = new HashMap<>();
+            Arrays.stream(ar).boxed().forEach(color -> {
+                colorsMap.computeIfAbsent(color, k -> Integer.valueOf(0));
+                colorsMap.computeIfPresent(color, (k, v) -> v + 1);
+            });
+            result = colorsMap.values().stream().reduce(Integer.valueOf(0), SockMerchant::divide);
+        }
+
+        return result;
+    }
+
+    private static Integer divide(Integer sum, Integer number) {
+        return Integer.sum(sum, Integer.divideUnsigned(number, 2));
     }
 }
+
