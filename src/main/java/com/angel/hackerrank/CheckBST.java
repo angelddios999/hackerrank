61c593db52d6b84ead06589b1033da46eec1cff9
Angel De Dios <angelddios999@gmail.com>
2018-12-16 21:40:56 -0800

    Updated CheckBST

--- src/main/java/com/angel/hackerrank/CheckBST.java
+++ src/main/java/com/angel/hackerrank/CheckBST.java
@@ -1,39 +1,18 @@
 package com.angel.hackerrank;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class CheckBST {
 
     boolean checkBST(Node root) {
-        List<Integer> values = new ArrayList<>();
-        return checkNode(root, values);
+        return checkNode(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 
-    private boolean checkNode(Node node, List<Integer> values) {
-        boolean isBST = true;
-
-        if(node != null) {
-            if(values.contains(node.data)) {
-                return false;
-            }
-
-            values.add(node.data);
-
-            if(node.left != null) {
-                if(node.left.data > node.data) {
-                    return false;
-                }
-            }
-            if(node.right != null) {
-                if(node.right.data < node.data) {
-                    return false;
-                }
-            }
-
-            isBST = checkNode(node.left, values) && checkNode(node.right, values);
+    private boolean checkNode(Node node, int min, int max) {
+        if(node == null) {
+            return true;
         }
 
-        return isBST;
+        return min < node.data && node.data < max
+                && checkNode(node.left, min, node.data)
+                && checkNode(node.right, node.data, max);
     }
 }
